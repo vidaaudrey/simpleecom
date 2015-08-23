242bd35e6a2ff6722df79c09e88ffe7ffc3465c8
vidaaudrey <audrey@33road.com>
2015-08-22 18:56:43 -0700

    minor ui update

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,6 +1,6 @@
 class Listing < ActiveRecord::Base
     if Rails.env.development?
-        has_attached_file :image, styles: { medium: "200x>", thumb: "100x100>" }, default_url: "/images/:style/missing.png", 
+        has_attached_file :image, styles: { medium: "200x>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
     else
         has_attached_file :image, styles: { medium: "200x>", thumb: "100x100>" }, default_url: "/images/:style/missing.png", 
             :storage => :dropbox,
